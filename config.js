0b1832e9071a6c9c858a7d786e380b7feff0993d
Xiguac <58496501+xiguac@users.noreply.github.com>
2022-03-18 14:47:29 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,25 @@
+// 配置
+window.Config = {
+
+  SiteName: 'Xiguac Website Status',
+
+  SiteUrl: 'https://status.oa5.xyz/',
+
+  ApiDomain: 'statusapi.xiguac.workers.dev',
+
+  ApiKeys: [
+    'ur1655125-0d64d060cd05a3a24895b8bb',
+  ],
+
+  ShowLink: true,
+
+  CountDays: 60,
+
+  // 导航栏菜单
+  Navi: [
+    {
+      text: 'Xiguac',
+      url: 'https://oa5.xyz/'
+    }
+  ]
+};
